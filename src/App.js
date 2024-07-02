c79e9671199067cc2c8820232c1600ca4d6da3da
Frankie Mundo <71039075+Frankie59@users.noreply.github.com>
2024-07-02 15:21:51 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import UpcomingProjects from './UpcomingProjects'; // Import the new component
 
 function App() {
   return (
@@ -10,11 +11,9 @@ function App() {
         {/* Navigation */}
         <nav className="flex justify-between p-4 bg-opacity-75 bg-gray-900 text-white">
           <div className="flex items-center">
-            {/* Adjust the width and height of the logo */}
             <img src="/logo.png" alt="My Logo" className="w-12 h-12" />
           </div>
           <div className="flex space-x-4">
-            {/* Updated styling for the buttons */}
             <Link
               to="/"
               className="hover:underline text-lg px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-md transition duration-300"
@@ -34,6 +33,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/upcomingprojects" element={<UpcomingProjects />} /> {/* Add new route */}
         </Routes>
       </div>
     </Router>
